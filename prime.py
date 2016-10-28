b9c56169d92b5e606f7895eed8a04fc8731f56e2
GDP254 <stvnkrs@gmail.com>
2016-10-28 17:45:02 +0300

    Addition of important comments

--- prime.py
+++ prime.py
@@ -3,6 +3,7 @@ def getPrime(num):
 	#Start from one since 0 is not a prime number
 	count = 1
 	while count <= num:
+		# is the modulus is beyond zero it is prime
 		if count % 2 > 0:
 			print(count)
 		count += 2
